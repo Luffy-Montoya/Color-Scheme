6adf717c86c269f9eb2335e101e455bcf8ba7c09
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-05-07 16:18:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const scheme = document.getElementById("scheme-container")
 const hexNumber = document.getElementById("hex-number")
 const splotch1 = document.getElementById("splotch-1")
 const splotch2 = document.getElementById("splotch-2")
+const clickToCopy = [hexNumber, splotch1, splotch2]
+let isHexBaseColor = true
 let htmlContent = ""
 
 function baseSplotch() {
     hexNumber.textContent = `Base ${baseColor.value.toUpperCase()}`
     splotch1.style.background = splotch2.style.background = baseColor.value
+    isHexBaseColor = true
 }
 
 function renderColors(data) {
@@ -31,10 +34,28 @@ function renderColors(data) {
         color.addEventListener("click", function(){
             hexNumber.textContent = `${hex}`
             splotch1.style.background = splotch2.style.background = hex
+            isHexBaseColor = false
         })  
     }
 
     baseSplotch()
+}
+
+function copyHex() {
+    if (isHexBaseColor === true) {
+        navigator.clipboard.writeText(hexNumber.textContent.replace("Base ", ""))
+        hexNumber.textContent = "Copied!"
+        setTimeout(() => {
+            hexNumber.textContent = `Base ${baseColor.value}`
+        }, 750)
+    } else {
+        let tempText = hexNumber.textContent
+        navigator.clipboard.writeText(hexNumber.textContent)
+        hexNumber.textContent = "Copied!"
+        setTimeout(() => {
+            hexNumber.textContent = tempText
+        }, 750)
+    }
 }
 
 fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor.value.replace("#", "")}&count=${count.value}&mode=${mode.value}`)
@@ -52,5 +73,5 @@ form.addEventListener("submit", function(e) {
     
 })
 
-baseColor.addEventListener("click", () =>
-    {baseSplotch()})
+baseColor.addEventListener("click", baseSplotch)
+clickToCopy.forEach((e) => e.addEventListener("click", copyHex))
